e43e107e000afe247d46f2ede263d35f27b1a21b
Haris khan <haristauseefkhan@gmail.com>
2023-02-15 23:38:57 +0500

    Initial commit

---
+++ src/test/java/com/eshop/EShop/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.EShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
